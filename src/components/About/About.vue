6ae5b15d899e09e319499fd56b6606acff7daf39
ricky <iloveyesterday@outlook.com>
2017-08-30 13:42:02 +0800

    base

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="about">
-
+    <h3>教室展示</h3>
+    <div class="class-room">
+      <img class="class-item" src="./1.jpg" alt="">
+      <img class="class-item" src="./1.jpg" alt="">
+      <img class="class-item" src="./1.jpg" alt="">
+      <img class="class-item" src="./1.jpg" alt="">
+      <img class="class-item" src="./1.jpg" alt="">
+      <img class="class-item" src="./1.jpg" alt="">
+      <img class="class-item" src="./1.jpg" alt="">
+      <img class="class-item" src="./1.jpg" alt="">
+      <img class="class-item" src="./1.jpg" alt="">
+      <img class="class-item" src="./1.jpg" alt="">
+    </div>
+    <h3>学员展示</h3>
+    <div class="class-room">
+      <img class="class-item" src="./2.jpg" alt="">
+      <img class="class-item" src="./2.jpg" alt="">
+      <img class="class-item" src="./2.jpg" alt="">
+      <img class="class-item" src="./2.jpg" alt="">
+      <img class="class-item" src="./2.jpg" alt="">
+      <img class="class-item" src="./2.jpg" alt="">
+      <img class="class-item" src="./2.jpg" alt="">
+      <img class="class-item" src="./2.jpg" alt="">
+      <img class="class-item" src="./2.jpg" alt="">
+      <img class="class-item" src="./2.jpg" alt="">
+    </div>
   </div>
 </template>
 
@@ -9,5 +34,14 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .about
+    h3
+      margin-left 30px
+      font-size 2em
+    .class-room
+      text-align center
+      margin 0 auto
+      .class-item
+        width 550px
+        height 450px
 </style>
